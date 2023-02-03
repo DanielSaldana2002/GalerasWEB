40ad332cf78e46657df1cd7c6fc13a59ffd66d72
Daniel Saldaña <109616516+DanielSaldana2002@users.noreply.github.com>
2023-02-03 00:09:46 -0600

    V1.1 - Cambio de menu y informacion

--- Code/main.js
+++ Code/main.js
@@ -1,28 +1,3 @@
-function animationEnterComparativa(idTitle, idInfo, titles, info){
-        document.getElementById(idTitle).innerHTML = titles;
-        document.getElementById(idInfo).innerHTML = info;
-        var title = document.getElementById(idTitle);
-        var info =  document.getElementById(idInfo);
-        var informations = document.getElementById('information');
-        title.style.transition = '500ms';
-        title.style.textAlign = 'center';
-        title.style.marginBottom = '30px';
-        informations.style.transition = '1000ms';
-        informations.style.backgroundColor = 'yellow';
-}
-
-function animationOutComparativa(idTitle, idInfo){
-    document.getElementById(idTitle).innerHTML = "";
-    document.getElementById(idInfo).innerHTML = "";
-    var title = document.getElementById(idTitle);
-    var info =  document.getElementById(idInfo);
-    var informations = document.getElementById('information');
-    title.style.textAlign = 'left';
-    title.style.marginBottom = '0px';
-    info.style.marginTop = '0px';
-    informations.style.backgroundColor = 'white';
-}
-
 function apiCarta(){
   var requestOptions = {
     method: 'GET'
